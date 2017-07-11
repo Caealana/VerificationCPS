5d81b3f7ea74b894bdbb1f32a6534b0bd6b6f895
sabrina <sabrinasayasith@gmail.com>
2017-07-11 18:22:45 -0500

    create DFS class
    for forward dfs traversal of cfg using dep info

--- src/slicing/DFS.java
+++ src/slicing/DFS.java
@@ -1,5 +1,24 @@
 package slicing;
-//DFS traversal based on dependency info
-public class DFS {
 
+import graphRepresentation.ControlFlowGraph;
+import graphRepresentation.Node;
+
+import java.util.Stack;
+
+//forward DFS traversal based on dependency info
+public class DFS {
+	collectDep depList;
+	Stack<Node> stack;
+	ControlFlowGraph cfg;
+	
+	public DFS(collectDep depList, ControlFlowGraph cfg){
+		this.depList = depList;
+		this.stack = new Stack<Node>();
+		this.cfg = cfg;
+	}
+	
+	//how should we stop traversal?
+	//we come to a branch statement and decide which one to take?
+	//also some nodes omitted due to slicing
+	
 }
