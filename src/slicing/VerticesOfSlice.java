995d5922c5760b20273fc6975ebb339e9218d31a
sabrina <sabrinasayasith@gmail.com>
2017-07-24 14:44:56 -0500

    VerticeOfSlice sets are set up

--- src/slicing/VerticesOfSlice.java
+++ src/slicing/VerticesOfSlice.java
@@ -2,6 +2,7 @@ package slicing;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Set;
 import java.util.Stack;
@@ -26,12 +27,21 @@ public class VerticesOfSlice {
 		marked = new ArrayList<Vertice>();
 	}
 	
+	public Set<Vertice> buildVerticesOfSlice(){
+		//Phase1: Slice without decending into called procedures
+		//V' = ReachingVertices(G, V, [def-order, parameter-out])
+		
+		//Phase2: Slice called procedures without ascending to call sites
+		//Answer := ReachingVertices(G, V', {def-order, parameter-in, call}})
+		return null;
+	}
+	
 	public Set<Vertice> ReachingVertices(){
 		//G an SDG
 		//V, WorkList, Answer, sets of vertices of G
 		//ExcludedEdgeKinds: a set of edge-kinds
 		//v, w: vertices of G
-		Set<Vertice> WorkList;
+		Set<Vertice> WorkList; //difficult to remove from set in java, start with arraylist
 		Set<Vertice> Answer;
 		Set<EdgeKind> ExcludedEdgesKinds;
 		Vertice v;
@@ -39,6 +49,8 @@ public class VerticesOfSlice {
 		
 		WorkList = V;
 		Answer = Collections.emptySet();
+		Object[] WorkListArr = WorkList.toArray();
+		ArrayList<Object> WorkListArrList = new ArrayList<Object>(Arrays.asList(WorkListArr));
 		while(WorkList.isEmpty() == false){
 			//select and remove a vertex v from WorkList
 			
